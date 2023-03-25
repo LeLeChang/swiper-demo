ee3b54a69d0bc708b5dcecc725de0497a85d7490
elena.chang <elena.chang@lativ.com.tw>
2023-03-25 10:35:03 +0800

    column Count depending on screen size
    column Count depending on screen size

--- src/app/waterfall/waterfall.component.ts
+++ src/app/waterfall/waterfall.component.ts
@@ -1,5 +1,11 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+} from '@angular/core';
 import { SingleWaterfallInfo } from '../model/home/home';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-waterfall',
@@ -9,4 +15,21 @@ import { SingleWaterfallInfo } from '../model/home/home';
 })
 export class WaterfallComponent {
   @Input() waterfall!: SingleWaterfallInfo;
+  isTwoColumn$ = new BehaviorSubject(true);
+
+  constructor(private responsive: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.responsive
+      .observe([Breakpoints.XSmall, Breakpoints.Small])
+      .subscribe((result) => {
+        const breakPoints = result.breakpoints;
+
+        if (breakPoints[Breakpoints.XSmall] || breakPoints[Breakpoints.Small]) {
+          this.isTwoColumn$.next(false);
+        }else{
+          this.isTwoColumn$.next(true);
+        }
+      });
+  }
 }
